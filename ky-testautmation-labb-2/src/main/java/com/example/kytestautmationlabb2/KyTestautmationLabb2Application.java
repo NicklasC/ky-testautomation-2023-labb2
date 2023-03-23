13078ed86aeec8c97962d4253e5be9d6f2e9f0fa
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-03-23 11:58:48 +0100

    Add initial test. Update .gitignore

---
+++ ky-testautmation-labb-2/src/main/java/com/example/kytestautmationlabb2/KyTestautmationLabb2Application.java
@@ -0,0 +1,13 @@
+package com.example.kytestautmationlabb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyTestautmationLabb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyTestautmationLabb2Application.class, args);
+	}
+
+}
