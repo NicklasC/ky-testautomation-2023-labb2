25ac843b97ded56f17b00a8e69e41be2a4bb5b61
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-03-26 20:37:03 +0200

    Add VG level tests

--- ky-testautmation-labb-2/src/test/java/com/example/kytestautmationlabb2/SeleniumTests.java
+++ ky-testautmation-labb-2/src/test/java/com/example/kytestautmationlabb2/SeleniumTests.java
@@ -47,11 +47,6 @@ class SeleniumTests {
 
         // TODO: Here we are doing an ugly wait to wait for the "saving your selection" after clicking 'acceptera alla' button above. Can be improved at some point.
         sleep(5);
-        /*try {
-            Thread.sleep(5000); // Sleep for 5 seconds (5000 milliseconds)
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
     }
 
     @AfterAll
@@ -64,6 +59,7 @@ class SeleniumTests {
         driver.get("https://svtplay.se");
     }
 
+    // Test 1: Kontrollera att webbplatsens titel stämmer
     @Test
     public void testWebsiteTitle() {
         // Navigera in till den URL du vill testa för respektive driver
@@ -74,8 +70,6 @@ class SeleniumTests {
     @Test
     // Test 2:  Kontrollera att webbplatsens logotyp är synlig
     public void testLogotypeIsVisible() {
-        //WebElement logo = driver.findElement(By.xpath("//div[@class=\"banner__image\" and @style=\"blahblah\"]"));
-
         WebElement logo = driver.findElement(By.xpath("//a[@data-rt-spec=\"play-logo\" and @href=\"/\"]"));
         assertEquals(true, logo.isDisplayed(), "I could not find the logo..");
     }
@@ -133,91 +127,88 @@ class SeleniumTests {
 
     // 7.1 Verify you can search on the site
     @Test
-    public void verifySearchLeadsToSearchResultPage(){
+    public void verifySearchLeadsToSearchResultPage() {
         WebElement searchField = driver.findElement(By.id("search"));
         WebElement searchButton = driver.findElement(By.xpath("//button[@type='submit' and @title='Sök på svtplay.se']"));
 
         searchField.sendKeys("ag");
         searchButton.click();
 
-
         WebElement searchResult = driver.findElement(By.xpath("//h2[@data-rt='header-search-result']"));
 
         String searchResultText = searchResult.getText();
-        String searchResultTextSubSelection = searchResultText.substring(0,21);
+        String searchResultTextSubSelection = searchResultText.substring(0, 21);
 
-        assertEquals("Din sökning på ag gav",searchResultTextSubSelection,"Some form of search results seems to display");
-        assertEquals("https://www.svtplay.se/sok?q=ag",driver.getCurrentUrl(),"Expected search result page URL is not correct)");
-
-        sleep(5);
+        assertEquals("Din sökning på ag gav", searchResultTextSubSelection, "Some form of search results seems to display");
+        assertEquals("https://www.svtplay.se/sok?q=ag", driver.getCurrentUrl(), "Expected search result page URL is not correct)");
     }
 
     // 7.2 Verify cookies information link displays.
     @Test
-    public void cookieInformationLinkDisplays(){
+    public void cookieInformationLinkDisplays() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/cookies-och-personuppgifter']"));
         String text = link.getText();
-        String textSelection = text.substring(0,30);
+        String textSelection = text.substring(0, 30);
 
-        assertEquals("Om cookies och personuppgifter",textSelection,"cookie information link text is not correct");
+        assertEquals("Om cookies och personuppgifter", textSelection, "cookie information link text is not correct");
     }
 
     // 7.3 Verify cookies information page displays.
     @Test
-    public void cookieInformationLinkLeadsToCorrectPage(){
+    public void cookieInformationLinkLeadsToCorrectPage() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/cookies-och-personuppgifter']"));
         link.click();
 
         WebElement header = driver.findElement(By.className("text-3xl"));
 
-        assertEquals("Om cookies och personuppgiftsbehandling på SVT",header.getText(),"cookie information header text is not correct");
+        assertEquals("Om cookies och personuppgiftsbehandling på SVT", header.getText(), "cookie information header text is not correct");
     }
 
     // 7.4.1 Verify TV tablå for channels displays
     @Test
-    public void TVTablåForSVT1Displays(){
+    public void TVTablåForSVT1Displays() {
         driver.get(("https://www.svtplay.se/kanaler"));
         WebElement section = driver.findElement(By.cssSelector("section[aria-label='Tablå för SVT 1']"));
-        assertEquals(true,section.isDisplayed(),"Tablå for SVT1 did not display");
+        assertEquals(true, section.isDisplayed(), "Tablå for SVT1 did not display");
     }
 
     // 7.4.2
     @Test
-    public void TVTablåForSVT2Displays(){
+    public void TVTablåForSVT2Displays() {
         driver.get(("https://www.svtplay.se/kanaler"));
         WebElement section = driver.findElement(By.cssSelector("section[aria-label='Tablå för SVT 2']"));
-        assertEquals(true,section.isDisplayed(),"Tablå for SVT2 did not display");
+        assertEquals(true, section.isDisplayed(), "Tablå for SVT2 did not display");
     }
 
     //7.4.3
     @Test
-    public void TVTablåForSVTBarnDisplays(){
+    public void TVTablåForSVTBarnDisplays() {
         driver.get(("https://www.svtplay.se/kanaler"));
         WebElement section = driver.findElement(By.cssSelector("section[aria-label='Tablå för SVT Barn']"));
-        assertEquals(true,section.isDisplayed(),"Tablå for SVT Barn did not display");
+        assertEquals(true, section.isDisplayed(), "Tablå for SVT Barn did not display");
     }
 
     //7.4.4
     @Test
-    public void TVTablåForKunskapskanalenDisplays(){
+    public void TVTablåForKunskapskanalenDisplays() {
         driver.get(("https://www.svtplay.se/kanaler"));
         WebElement section = driver.findElement(By.cssSelector("section[aria-label='Tablå för Kunskapskanalen']"));
-        assertEquals(true,section.isDisplayed(),"Tablå for Kunskapskanalen did not display");
+        assertEquals(true, section.isDisplayed(), "Tablå for Kunskapskanalen did not display");
     }
 
     // 7.5.1
     @Test
-    public void NyhetsbrevLinkDisplays(){
+    public void NyhetsbrevLinkDisplays() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://nyhetsbrev.svtplay.se/prenumerera/?utm_source=svtplay&utm_medium=footer-cta']"));
         String text = link.getText();
-        String textSelection = text.substring(0,10);
+        String textSelection = text.substring(0, 10);
 
-        assertEquals("Nyhetsbrev",textSelection,"Nyhetsbrev link text is not correct");
+        assertEquals("Nyhetsbrev", textSelection, "Nyhetsbrev link text is not correct");
     }
 
     // 7.5.2 Verify cookies information page displays.
     @Test
-    public void NyhetsbrevLinkLeadsToCorrectPage(){
+    public void NyhetsbrevLinkLeadsToCorrectPage() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://nyhetsbrev.svtplay.se/prenumerera/?utm_source=svtplay&utm_medium=footer-cta']"));
         link.click();
 
@@ -225,16 +216,121 @@ class SeleniumTests {
 
         //String headerText=header.getText();
 
-        assertEquals("Missa inga program och serier!",header.getText(),"nyhetsbrev title page not correct");
+        assertEquals("Missa inga program och serier!", header.getText(), "nyhetsbrev title page not correct");
+    }
+
+    // 8.1 (VG) Testa sökformuläret genom att söka efter “Agenda” och kontrollera att
+    // programmet Agenda dyker upp överst i sökresultatet. Sökformuläret ska
+    //  lokaliseras med locatorn för name-attributet.
+
+    @Test
+    public void searchForAgenda() {
+        WebElement searchField = driver.findElement(By.cssSelector("input[name='q']"));
+        WebElement searchButton = driver.findElement(By.xpath("//button[@type='submit' and @title='Sök på svtplay.se']"));
+
+        searchField.sendKeys("Agenda");
+        searchButton.click();
+
+        // Getting all H2 elements
+        List<WebElement> h2Elements = driver.findElements(By.tagName("h2"));
+
+        //Getting search result header text
+        String searchResultHeaderText = h2Elements.get(0).getText();
+
+        //Getting a subselection only, as we do not know exactly how many search hits we will get.
+        String searchResultTextSubSelection = searchResultHeaderText.substring(0, 21);
+
+        assertEquals("Din sökning på Agenda", searchResultTextSubSelection, "search for Agenda header presented");
+
+        // Verifying the first search result is Agenda.
+        String firstSearchResultText = h2Elements.get(1).getText();
+        assertEquals("Agenda", firstSearchResultText, "First search result should be Agenda, but was not");
+
+        /*for (WebElement element : h2Elements) {
+            // Writes all h2 elements text
+            System.out.println(element.getText());
+        }*/
+    }
+
+    // 8.2 (VG) Använd sökförmuläret för att navigera in till programsidan för programmet
+    // “Pistvakt”. Kontrollera därefter antalet program i säsong 2 av serien. Kontrollera
+    // även namnet på avsnitt 5 i säsong 2
+
+    @Test
+    public void searchForPistvaktAndVerifyNumberOfEpisodesAndEpisodeName() {
+        WebElement searchField = driver.findElement(By.cssSelector("input[name='q']"));
+        WebElement searchButton = driver.findElement(By.xpath("//button[@type='submit' and @title='Sök på svtplay.se']"));
+
+        searchField.sendKeys("Pistvakt");
+        searchButton.click();
+
+        // Getting all H2 object
+        List<WebElement> h2Elements = driver.findElements(By.tagName("h2"));
+
+        // Clicking on Pistvakt
+        WebElement pistvaktElement = h2Elements.get(1);
+        pistvaktElement.click();
+
+        // Initially Season 1 is expanded, but not season 2. Because of this I click on season 1 button to collapse it, and then season 2 to expand it. That way, only season 2 episodes should show.
+
+        WebElement selectSeason1link = driver.findElement(By.cssSelector("a[href='/pistvakt']"));
+        selectSeason1link.click();
+        // Both Season 1 and Season 2 should now be collapsed
+
+        // Clicking on Season 2 => It should expand
+        WebElement selectSeason2link = driver.findElement(By.cssSelector("a[href='/pistvakt?tabs=season-2-jx3za5B']"));
+        selectSeason2link.click();
+
+        // Now only season 2 episodes are listed. Fetching the season 2 episodes into a list
+
+
+        List<WebElement> season2EpisodesList = driver.findElements(By.tagName("h3"));
+
+        // Going through all H3 elements. We are only interested in elements that contains text. I did not know how to filter that using findElements, so I did it programatically instead.
+
+        int numberOfEpisodes = 0;
+        for (WebElement episode : season2EpisodesList) {
+            if (episode.getText().length() > 0) {
+                numberOfEpisodes++;
+                // Writes all h3 elements text
+                System.out.println(episode.getText());
+            }
+        }
+
+        // Making requested asserts.
+        assertEquals(6, numberOfEpisodes, "Incorrect amount of season 2 number of episodes");
+        assertEquals("5. Personalfestan", season2EpisodesList.get(4).getText(), "Season 2 Episode 5 name should be 5. Personalfestan, but it is not");
     }
 
 
+    // 8.3 Skriv ytterligare 5 olika test för SVT Play med relevanta kontroller
+    //
+    // Nicklas kommentar: Har skrivit 4 ytterligare tester i föregående steg, dvs
+    // 7.4.2
+    // 7.4.3
+    // 7.4.4
+    // 7.5.2
 
+    // Making an additional test (Actually with two verification points) so we have 5 extra tests for VG level.
+    @Test
+    public void DatorLinkDisplaysAndNavigatesToCorrectPage() {
+        WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/svt-play-webb']"));
+        String text = link.getText();
+        String textSelection = text.substring(0, 5);
 
+        assertEquals("Dator", textSelection, "Dator information link text starts with Dator, OK!");
 
-    public static void sleep(int numberOfSeconds){
+        link.click();
+        WebElement header = driver.findElement(By.cssSelector("H1"));
+
+        assertEquals("Systemkrav för SVT Play på webben", header.getText(), "Link 'Dator' did not lead to a page with correct text.");
+        System.out.println(header.getText());
+
+    }
+
+    public static void sleep(int numberOfSeconds) {
         try {
-            Thread.sleep(numberOfSeconds * 1000); // Sleep for 5 seconds (5000 milliseconds)
+            Thread.sleep(numberOfSeconds * 1000); // Sleep for number of seconds seconds
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
